3444bc91719fd63c566a31fe9a55717080a728d1
zxk105 <zxk@tingood.com>
2018-03-14 04:00:02 +0000

    small modify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Zxk</div>
+                <div class="title">Hello Laravel! - by Zxk1052</div>
             </div>
         </div>
     </body>
